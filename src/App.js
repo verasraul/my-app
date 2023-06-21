3df0532a9e62ee5284001b0da145bf8c905a49bc
Raul Veras <verasraul@gmail.com>
2023-06-21 01:36:03 -0400

    adding src modules

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <Product title="Apple Airpods" description="Lorem ipsum dolor sit amet, graecis intellegat pertinacia cum et" 
+      image="https://cdn0.iconfinder.com/data/icons/household-items-2/100/airpod2-512.png" />
       </header>
     </div>
   );
